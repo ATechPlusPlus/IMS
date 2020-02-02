776d3a9561faecadd1582e058c13bbebe0049e84
Abdul Mateen Khan <abdulmateen50@gmail.com>
2020-02-02 17:05:37 +0530

    Design changes, created few folders for respective forms

---
+++ IMS/Settings/Employee_Commission_Setting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IMS.Settings
+{
+    public partial class Employee_Commission_Setting : Form
+    {
+        public Employee_Commission_Setting()
+        {
+            InitializeComponent();
+        }
+    }
+}
